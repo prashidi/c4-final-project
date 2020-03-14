e15b946086036dc67dcdab9682fa66566658dc8c
Patrick Rashidi <prashidi174@gmail.com>
2020-03-14 12:41:49 +0200

    feat: added deleteTodo

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,37 @@
-// import 'source-map-support/register'
+import "source-map-support/register";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from "aws-lambda";
+import * as AWS from "aws-sdk";
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+const tableName = process.env.TODOS_TABLE;
 
-//   // TODO: Remove a TODO item by id
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId;
+
+  await docClient
+    .delete({
+      TableName: tableName,
+      Key: {
+        todoId
+      }
+    })
+    .promise();
+
+  return {
+    statusCode: 202,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify({
+      success: true
+    })
+  };
+};
