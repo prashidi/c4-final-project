3412c1dde236480864feec5db5455aa5e2544bf7
Patrick Rashidi <prashidi174@gmail.com>
2020-03-16 18:09:28 +0200

    feat: setup serverless yml configb file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 's0h8h2k87b'
+const apiId = 'th23q6wx35'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
